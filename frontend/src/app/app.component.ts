1e6517330aabba0c2ed2c8365aa365f4d3094796
Deepa Peri <Deepa.Peri@accolite.com>
2019-07-01 14:07:55 +0530

    Bootstrap initiated frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { setTheme } from 'ngx-bootstrap/utils';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  constructor(){
+    setTheme('bs3'); // or 'bs4'
+  }
+  title = 'aums';
 }
