5a59a7919f2b2cddae2ad5aeb8c56bbff1abd109
Kotha Srujanvesh <kotha.srujanvesh@accolite.com>
2019-07-04 18:17:07 +0530

    added course management ui

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CoursesModule } from './courses/courses.module';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'', loadChildren: () => CoursesModule,pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CoursesModule } from './courses/courses.module';
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+import { MatSelectModule } from '@angular/material/select';
+import * as moment from 'moment';
+import { MultiSelectAllModule } from '@syncfusion/ej2-angular-dropdowns';
+import { AngularDateTimePickerModule} from 'angular2-datetimepicker';
+import {HeaderComponent} from '../app/courses/header/header.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +19,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoursesModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+   NgMultiSelectDropDownModule.forRoot(),
+   MatSelectModule,
+   MultiSelectAllModule,
+   AngularDateTimePickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
