b56af60340abd4c4e9ee3254232e4333d9fbb7b6
Deepa Peri <Deepa.Peri@accolite.com>
2019-07-05 04:36:10 +0530

    Please enter the commit message for your changes. Lines starting deleted the backend files

--- backend/grads/src/main/java/com/accolite/grads/exception/GradNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.accolite.grads.exception;
-
-public class GradNotFoundException extends RuntimeException {
-	public GradNotFoundException(Long id) {
-		System.out.println("Cannot find Grad with id: " + id);
-	}
-}
