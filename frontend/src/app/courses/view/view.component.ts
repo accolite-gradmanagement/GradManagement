89ca01b6c92402fd12da3067eed52d64476b1452
Alex Pandian <alex.pandian@accolite.com>
2019-07-09 14:12:11 +0530

    styling

--- frontend/src/app/courses/view/view.component.ts
+++ frontend/src/app/courses/view/view.component.ts
@@ -1,14 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { TrainerAllocation, Batch } from './RequiredClasses';
-
-import { ElementRef, TemplateRef } from '@angular/core';
-// import {
-//   PopupService,
-//   PopupRef
-// } from '@progress/kendo-angular-popup';
 import { SharedService } from '../../shared.service';
-import { element } from 'protractor';
 
 const dateFormat = 'dd/MM/yyyy';
 @Component({
@@ -17,53 +10,48 @@ const dateFormat = 'dd/MM/yyyy';
     styleUrls: ['./view.component.css']
 })
 export class ViewComponent implements OnInit {
-
-    // sessions: TrainerAllocation[];
-    // updateTrainer: TrainerAllocation;
-    // private popupRef: PopupRef;
     batches:Batch[];
     oneBatch:Batch;
     sessions:TrainerAllocation[];
     selectedBatch:number;
     var1:boolean=false;
     var2:boolean=false;
-    //isDisabled=true;
     datePipe = new DatePipe('en-US');
     datewiseSessions;
+    keysOfDatewiseSessions;
+    selectedBatchName:string;
     // datewiseSessions = new Map<string, TrainerAllocation[]>();
 
     constructor(private service: SharedService) { }
     ngOnInit() {
         this.service.getBatches().subscribe(data=>{
             this.batches=data;
-            console.log(this.batches);
+            // console.log(this.batches);
         })
         this.selectedBatch=46;
         this.loadSessions();
-        // this.service.viewSessions().subscribe(data => {
-        //     this.sessions = data;
-        //     // console.log(this.sessions);
-        //     this.sortSessionsByDate();
-        //     this.createDateWiseSessions();
-        //     // console.log(this.datewiseSessions);
-        //     //this.isDisabled=false;
-        // })
     }
 
     loadSessions()
     {
+        console.log(this.selectedBatch);
         // console.log(this.selectedBatch);
         
+        // this.batches.forEach(element=>
+        //     {
+        //         if(this.selectedBatch==element.batchId)
+        //         this.selectedBatchName=element.batchName;
+        //     });
         this.datewiseSessions = new Map<string, TrainerAllocation[]>();
         this.service.getTimesheetForBatch(this.selectedBatch).subscribe(data=>
             {
                 this.oneBatch=data;
                 this.sessions=this.oneBatch.trainerAllocation;
-                console.log(this.sessions);
+                // console.log(this.sessions);
                 this.sessions.sort((a: TrainerAllocation, b: TrainerAllocation) => {
                     return <any>new Date(a.start_time) - <any>new Date(b.start_time);
                 });
-                console.log(this.sessions);
+                // console.log(this.sessions);
                 
                 this.sessions.forEach(element => {
                     if (this.datewiseSessions.has(this.datePipe.transform(element.start_time, dateFormat))) {
@@ -73,12 +61,16 @@ export class ViewComponent implements OnInit {
                         this.datewiseSessions.set(this.datePipe.transform(element.start_time, dateFormat), [element]);
                     }
                 });
-                console.log(this.datewiseSessions.keys());
+                this.keysOfDatewiseSessions=this.datewiseSessions.keys();
+
+                // console.log(this.datewiseSessions.keys());
 
                 if(this.sessions.length!=0)
                 {
                     this.var2=false;
                     this.var1=true;
+                    console.log(this.keysOfDatewiseSessions);
+
                 }
                 else
                 {
@@ -86,52 +78,12 @@ export class ViewComponent implements OnInit {
                     this.var2=true;
                 }
 
-            })
-
-    
+            })    
     }
     public config:any = {
         paging: true,
         filtering: {filterString: ''},
         className: ['table-striped', 'table-bordered']
       };
-
-    // public sortSessionsByDate(): void {
-    //     this.sessions.sort((a: TrainerAllocation, b: TrainerAllocation) => {
-    //         return <any>a.start_time - <any>b.start_time;
-    //     });
-    // }
-
-    // public createDateWiseSessions() {
-    //     this.sessions.forEach(element => {
-    //         if (this.datewiseSessions.has(this.datePipe.transform(element.start_time, dateFormat))) {
-    //             this.datewiseSessions.get(this.datePipe.transform(element.start_time, dateFormat)).push(element);
-    //         }
-    //         else {
-    //             this.datewiseSessions.set(this.datePipe.transform(element.start_time, dateFormat), [element]);
-    //         }
-    //     });
-    // }
-
-    redirectToAdd(tid: number) {
-        this.service.getObject(tid).subscribe(data=>
-            {
-                this.ngOnInit();
-            });
-        // this.sessions.forEach(data=>this.sessions.pop());
-        // this.sessions.forEach(data=>
-        //     if(data.trainer_allocation_id==tid){
-        //         this.sessions.
-        //     }
-        //     );
-        // this.service.viewSessions().subscribe(data => {
-        //     this.sessions = data;
-        //     console.log(this.sessions);
-        //     this.sortSessionsByDate();
-        //     this.createDateWiseSessions();
-        //     console.log(this.datewiseSessions);
-        //this.isDisabled=false;
-        // })
-    }
 
 }
