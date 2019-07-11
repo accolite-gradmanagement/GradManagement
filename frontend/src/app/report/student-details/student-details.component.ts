8e67090ca2d0cfaa0588cbb8bf03d6b2bb509ec1
Kanchan Kumari <kanchan.kumari@accolite.com>
2019-07-11 12:58:49 +0530

    ag grid

--- frontend/src/app/report/student-details/student-details.component.ts
+++ frontend/src/app/report/student-details/student-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ReportService } from '../../report.service';
 import { Student } from '../../student';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-student-details',
@@ -9,13 +10,22 @@ import { Student } from '../../student';
   styleUrls: ['./student-details.component.css']
 })
 export class StudentDetailsComponent implements OnInit {
+  
+  columnDefs = [
+    {headerName: 'TestId', field: 'testId' },
+    {headerName: 'Test Name', field: 'testName' },
+    {headerName: 'Score', field: 'score'},
+    {headerName: 'Rank', field: 'rank'}
+   ];
+   rowData: Student[];
+
   istable: boolean=false;
   nameSearch: string;
   contents:Student[];
   private isLoaded:boolean=false;
   constructor(private reportService: ReportService,
 
-    private route:ActivatedRoute) { }
+    private route:ActivatedRoute,private http: HttpClient) { }
 
   ngOnInit() {
   const id = this.route.snapshot.paramMap.get('id');
@@ -58,8 +68,8 @@ getDetails(id:string){
       else
       {
         // this.router.navigate(["score/detail/name",this.nameSearch]);
-        this.contents = data;
-        console.log(this.contents);
+        this.rowData = data;
+        console.log(this.rowData);
         this.isLoaded=true; 
         this.istable=true;
       }
